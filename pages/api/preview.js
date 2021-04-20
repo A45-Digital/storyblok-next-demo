94f810e7e5364a3cec9b0f48d7bb559c1fc422c5
Bernhard <bernhard@a45.li>
2021-04-20 10:33:44 +0200

    Update preview.js

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -10,13 +10,13 @@ export default async function preview(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // Fetch the headless CMS to check if the provided `slug` exists
-  const post = await getPreviewPostBySlug(req.query.slug)
+  // // Fetch the headless CMS to check if the provided `slug` exists
+  // const post = await getPreviewPostBySlug(req.query.slug)
 
-  // If the slug doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' })
-  }
+  // // If the slug doesn't exist prevent preview mode from being enabled
+  // if (!post) {
+  //   return res.status(401).json({ message: 'Invalid slug' })
+  // }
 
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
@@ -27,6 +27,6 @@ export default async function preview(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/posts/${post?.PostItem?.slug}` })
+  res.writeHead(307, { Location: `/posts/${req.query.slug}` })
   res.end()
 }
