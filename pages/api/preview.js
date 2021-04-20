3911b6243856aea8336891004a329a899b0c61bb
Bernhard <bernhard@a45.li>
2021-04-20 12:46:54 +0200

    Update preview.js

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,6 +1,5 @@
-import { getPreviewPostBySlug } from '@/lib/api'
-
 export default async function preview(req, res) {
+  console.log('DEBUG: preview(): activate preview mode')
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
   if (
@@ -10,14 +9,6 @@ export default async function preview(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // // Fetch the headless CMS to check if the provided `slug` exists
-  // const post = await getPreviewPostBySlug(req.query.slug)
-
-  // // If the slug doesn't exist prevent preview mode from being enabled
-  // if (!post) {
-  //   return res.status(401).json({ message: 'Invalid slug' })
-  // }
-
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
 
@@ -25,8 +16,9 @@ export default async function preview(req, res) {
   const cookies = res.getHeader('Set-Cookie')
   res.setHeader('Set-Cookie', cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None')))
 
+  console.log('DEBUG: preview(): redirect to:', `/${req.query.slug}`)
+
   // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/${req.query.slug}` })
+  res.redirect(`/${req.query.slug}`)
   res.end()
 }
